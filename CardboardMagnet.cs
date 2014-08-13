a5edc7307fccfd13f9083a1ecee9a6d0fe4507e6
Borders, Casey <casey.borders@gsw-w.com>
2014-08-13 15:57:10 -0400

    Naïve implementation

---
+++ CardboardMagnet.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CardboardMagnet : MonoBehaviour
+{
+	public class MagnetListener : MonoBehaviour
+	{
+		public virtual void OnMagnetTriggered() {}
+	}
+
+	public int _sampleSize = 40;
+	public int _lowTrigger = 30;
+	public int _highTrigger = 30;
+
+	public MagnetListener _listener;
+
+	private float _current, _mean, _tmp;
+	private Queue<float> _window;
+
+	void Start()
+	{
+		_window = new Queue<float> (_sampleSize + 1);
+	}
+
+	void Update ()
+	{
+		// Add most recent reading
+		_current = Input.compass.magneticHeading;
+		_window.Enqueue(_current);
+
+		// If we don't have enough samples, keep reading
+		if(_window.Count < _sampleSize) return;
+		// If you have more samples than we need get rid of the oldest one
+		if(_window.Count > _sampleSize) _window.Dequeue();
+
+		_mean = FindMean(_window);
+
+		Debug.Log("----------------------------------------");
+		Debug.Log("Current: " + _current);
+		Debug.Log("Mean: " + _mean);
+		Debug.Log("High Trigger: " + _highTrigger);
+		Debug.Log("Low Trigger: " + _lowTrigger);
+
+		if(_current < _mean - _lowTrigger || _current > _mean + _highTrigger)
+		{
+			Debug.Log("!!! FIRE !!!");
+			if(_listener != null) _listener.OnMagnetTriggered();
+		}
+	}
+
+	private float FindMean(Queue<float> range)
+	{
+		_tmp = 0;
+		foreach(float heading in range)
+		{
+			_tmp += heading;
+		}
+		return _tmp / range.Count;
+	}
+}
